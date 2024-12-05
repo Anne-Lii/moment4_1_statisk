6c4c5d8f6cd24468c4d5fccba334ab1afd39e139
Anne-Lii <145770813+Anne-Lii@users.noreply.github.com>
2024-12-05 14:18:38 +0100

    lagt till meny för mindre skärmar samt layout för mindre skärmar

--- js/main.js
+++ js/main.js
@@ -4,6 +4,10 @@ const categoryLinks = document.querySelectorAll(".sidebar a");
 // Hämta alla produkter
 const products = document.querySelectorAll(".product");
 
+// Hämta hamburgerknappen och navigationslistan
+const hamburger = document.querySelector('.hamburger');
+const navMenu = document.querySelector('.main_nav ul');
+
 //Lägger till klick-event på kategorilänkarna
 categoryLinks.forEach(link =>{
     link.addEventListener("click", (e) => {
@@ -21,3 +25,16 @@ categoryLinks.forEach(link =>{
         });
     });
 });
+
+// Kontrollerar att elementen hittas
+if (hamburger && navMenu) {
+    
+    // Lägger till klickhändelse för att toggla menyn
+    hamburger.addEventListener('click', () => {
+        
+        navMenu.classList.toggle('active');
+       
+    });
+} else {
+    console.error("Hamburger eller navigeringsmenyn kunde inte hittas!");
+}
